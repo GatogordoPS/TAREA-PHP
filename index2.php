a4df39131980a2d3ee176a2fceb9779429b6437e
GatogordoPS <155909261+GatogordoPS@users.noreply.github.com>
2026-03-17 22:19:42 +0100

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Proyecto</title>
+        <link rel="stylesheet" href="main.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    </head>
+    <body class="content">
+        <div class="Agradece">
+         <img src="./agradece.png">   
+        </div>
+        <nav>
+            <a href="#Agradecer">Agradecer</a>
+	        <a href="#Recibir">Recibir</a>
+	        <a href="#Cerrar">Cerrar Sesion</a>
+        </nav>
+        <form class="form2" action="">
+            <p>Para</p>
+            <!-- <php ?-->
+            <select>
+                <option> --- Selecciona Nombre --- </option>
+                <option value="1">Nico</option>
+                <!-- EL Value Sera el ID Alumno y el Texto Sera el Nombre del Alumno-->
+                 <!-- Se Tendran tantos options como filas alumnos-->
+            </select>
+            <p>Quiero Agradecerte</p>
+            <textarea class="texto" type="text"></textarea>
+        </form>
+    </body>
+</html>
