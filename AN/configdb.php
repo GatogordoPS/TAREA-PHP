d51c09e0ae6edce4ed64a7eb1080c60300fa0597
GatogordoPS <155909261+GatogordoPS@users.noreply.github.com>
2026-03-27 12:39:10 +0100

    Update database credentials in configdb.php

--- AN/configdb.php
+++ AN/configdb.php
@@ -1,14 +1,12 @@
 <?php
-
-define("SERVIDOR", "localhost");
-define("USUARIO", "root");
-define("PASSWORD", "");
-define("BBDD", "alumnos");
+define("SERVIDOR",'localhost');
+define("USUARIO",'root');
+define("PASSWORD",'');
+define("BBDD",'agradece2026');
 
 function conectar(){
     $conexion = new mysqli(SERVIDOR, USUARIO, PASSWORD, BBDD);
     $conexion->set_charset("utf8");
     return $conexion;
 }
-
 ?>
